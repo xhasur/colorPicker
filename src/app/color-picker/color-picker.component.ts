8312bb8247632a50239e6421fc248e9cbbccb381
xhasur <>
2020-07-14 16:41:38 -0500

    Se agrega  solucion de color Picker

--- src/app/color-picker/color-picker.component.ts
+++ src/app/color-picker/color-picker.component.ts
@@ -11,7 +11,12 @@ export class ColorPickerComponent implements OnInit {
   selectedColor : string;
 
   ngOnInit(): void {
+    this.selectedColor = this.initialColor;
   }
 
 
+ seleccionarColor(color: string){
+   this.selectedColor = color;
+ }
+ 
 }
